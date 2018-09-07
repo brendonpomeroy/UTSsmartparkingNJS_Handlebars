a314d26055cef3bb17ba3d2394f5c76f3e78d9ac
Brendon <brppomeroy@gmail.com>
2018-09-08 00:46:56 +1000

    Testing with handlebars

--- routes/bookSpace.js
+++ routes/bookSpace.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/bookSpace', function(req, res, next) {
-    res.render('bookSpace', { title: 'Book Space' });
+    res.render('bookSpace.hbs', { title: 'Book Space' });
 });
 
 module.exports = router;
