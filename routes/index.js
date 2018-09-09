d0841804319d7f72a588aa4028e6a7ffaf13fb7c
Brendon <brppomeroy@gmail.com>
2018-09-09 15:59:26 +1000

    Testing with handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,9 @@
 var express = require('express');
 var router = express.Router();
+var mongo = require('mongodb');
+var assert = require('assert');
+
+var url = "mongodb+srv://System:utssmartparking@parkdb-fez7r.mongodb.net/test?retryWrites=true";
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
@@ -16,6 +20,50 @@ router.get('/bookings', function(req, res, next) {
 
 router.get('/account', function(req, res, next) {
     res.render('account', { title: 'Account' });
+});
+
+
+router.get('/get-users', function(req, res, next) {
+    var users = [];
+    mongo.connect(url, function(err, db){
+        assert().equal(null, err);
+        var cursor = db.collection('users').find(); //essentially an iterator
+        cursor.forEach(function(doc, err) { // doc is the variable we want -> document (like an SQL entry)
+            assert.equal(null, err); // check for an error
+            users.push(doc); //add the document to the users array
+        }, function() {
+            db.close();// must be here due to node.js being asynchronous
+            res.render('manageUsers', { items: users});
+        });
+    });
+});
+
+router.post('/insert-user', function(req, res, next) {
+    var user = {
+        userID: req.body.userID,
+        name: req.body.name,
+        phone: req.body.phone,
+        email: req.body.email,
+        password: req.body.password,
+        usertype: req.body.usertype
+    };
+
+    mongo.connect(url, function(err, db){
+        assert().equal(null, err);
+        db.collection('users').insertOne(user, function(err, result) {
+            assert(null, error);
+            console.log('user added.');
+            db.close();
+        });
+    })
+});
+
+router.post('/update', function(req, res, next) {
+
+});
+
+router.post('/delete', function(req, res, next) {
+
 });
 
 module.exports = router;
