05b269d344081ff28806d892d4b72cd27796f8a1
Brendon <brppomeroy@gmail.com>
2018-09-09 12:41:09 +1000

    Testing with handlebars

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
+router.get('/bookSpace', function(req, res, next) {
+    res.render('bookSpace', { title: 'Book Space' });
+});
+
 module.exports = router;
