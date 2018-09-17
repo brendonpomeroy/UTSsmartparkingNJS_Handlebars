b809445eabbadd6ae74cb8f955095e4f25911f7e
Brendon <brppomeroy@gmail.com>
2018-09-17 10:29:28 +1000

    Updating login page

--- routes/index.js
+++ routes/index.js
@@ -48,7 +48,7 @@ router.post('/login', function(req, res, next) {
         const cursor = db.collection('Users').find({ userID: req.body.userID });
         if (cursor.userID == req.body.userID && cursor.password === req.body.password) {
             user = cursor;
-            res.render('dashboard', { status: 'success' });
+            res.render('dashboard', { status: user });
         }
     });
     res.render('dashboard', { status: 'fail' });
