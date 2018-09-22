52e38471d8d53e074549519b4a50787ed9508147
Brendon <brppomeroy@gmail.com>
2018-09-22 18:05:45 +1000

    Fix Booking

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,42 @@
 var express = require('express');
 var router = express.Router();
+var mongoose = require('mongoose');
+mongoose.connect("mongodb+srv://System:utssmartparking@parkdb-fez7r.mongodb.net/");
+
+var userSchema = mongoose.schema({
+    userID: int,
+    name: String,
+    phone: int,
+    email: String,
+    userType: String,
+    password: String
+});
+
+var userModel = mongoose.model('Users', userSchema);
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.render('dashboard', { status: "users router."});
+    res.render('/dashboard', { status: "users router."});
+});
+
+router.post('/addUser', function(req, res, next) {
+
+    var newUser = new userModel();
+    newUser.userID = parseInt(req.body.userID);
+    newUser.name = req.body.name;
+    newUser.phone = parseInt(req.body.phone);
+    newUser.email = req.body.email;
+    newUser.userType = req.body.userType;
+    newUser.password = req.body.password;
+
+    newUser.save(function(err, addedUser) {
+        if (err) {
+            console.log(err);
+            res.status(500).send();
+        } else {
+            res.render('/dashboard', { status: "success"} )
+        }
+    });
 });
 
 module.exports = router;
